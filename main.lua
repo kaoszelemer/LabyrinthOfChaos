f91e09cf44841460852f820e7afc4e09e1283a6b
Kaosz Elemer <attila.k84@gmail.com>
2021-02-28 21:14:03 +0100

    map, and collision

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 Camera = require("lib.humpcamera")
-mainMap = require("map1")
+mainMap = require("map2")
 love.graphics.setDefaultFilter( 'nearest', 'nearest' )
 width= 800
 height= 800
 
 white = {1,1,1}
+black = {0,0,0}
 
-
+font = love.graphics.newFont(8)
 
 tileSetAtlas = love.graphics.newImage("tileset.png")
 player = {}
@@ -31,92 +32,152 @@ local function loadQuads()
 
 	player.quad = love.graphics.newQuad(0, 0, tileW, tileH, tileSetAtlas:getWidth(), tileSetAtlas:getHeight())
 	tiles.wall = love.graphics.newQuad(24, 0, tileW, tileH, tileSetAtlas:getWidth(), tileSetAtlas:getHeight())
+	tiles.liltree = love.graphics.newQuad(8, 0, tileW, tileH, tileSetAtlas:getWidth(), tileSetAtlas:getHeight())
+	tiles.bigtree = love.graphics.newQuad(16, 0, tileW, tileH, tileSetAtlas:getWidth(), tileSetAtlas:getHeight())
+	tiles.sign = love.graphics.newQuad(32, 0, tileW, tileH, tileSetAtlas:getWidth(), tileSetAtlas:getHeight())
 
 end
 
 local function testMapForCollision(x, y)
-	if mainMap[(player.y / 8) + y][(player.x / 8) + x] > 0 then
-		return false
+	if (player.y / 8 + y > 0 and player.y / 8 + y <= 100) and (player.x / 8 + x > 0 and player.x / 8 + x <= 100) then
+	
+
+		if mainMap[(player.y / 8 + y)][player.x / 8 + x] > 0 then
+			return false
+		end
+		return true
 	end
-	return true
 end
 
 local function testMapForUse(x, y)
-	if mainMap[(player.y / 8) + y][(player.x / 8) + x] ~= 5 then
-		return false
+
+	if mainMap[(player.y / 8) + y][(player.x / 8) + x] == 5 then
+		return true
 	end
-	return true
+	return false
 end
 
 
 
 
-function love:load()
-	player.x = 24
-	player.y = 24
+function love.load()
+	love.graphics.setFont(font)
+	player.x = 34 * tileW
+	player.y = 94 * tileH
+	player.actx = 34 * tileW
+	player.acty = 94 * tileH
+	player.speed = 10
 	love.graphics.setDefaultFilter( 'nearest', 'nearest' ) -- for camera
 	loadQuads()
-    camera = Camera(player.x, player.y, 6)
+    camera = Camera(player.actx, player.acty, 6)
  
 end
 
-function love:update()
+function love.update(dt)
+	
 	local dx,dy = player.x - camera.x, player.y - camera.y
     camera:move(dx/2, dy/2)
+	player.acty = player.acty - ((player.acty - player.y) * player.speed * dt)
+	player.actx = player.actx - ((player.actx - player.x) * player.speed * dt)
+
+
 end
 
-function love:draw()
+function love.draw()
    camera:attach()
         -- draw camera stuff here
-      love.graphics.draw(tileSetAtlas, player.quad, player.x, player.y)
+    love.graphics.draw(tileSetAtlas, player.quad, player.actx, player.acty)
 
 	
-      for x = 1, #mainMap do
+    for x = 1, #mainMap do
 		for y = 1, #mainMap[x] do
+
 			if mainMap[y][x] == 4 then
 			love.graphics.draw(tileSetAtlas, tiles.wall, x * tileW, y * tileH)
+
+			end
+			if mainMap[y][x] == 2 then
+			love.graphics.draw(tileSetAtlas, tiles.liltree, x * tileW, y * tileH)
+
+			end
+			if mainMap[y][x] == 3 then
+			love.graphics.draw(tileSetAtlas, tiles.bigtree, x * tileW, y * tileH)
+
+			end
+			if mainMap[y][x] == 5 then
+			love.graphics.draw(tileSetAtlas, tiles.sign, x * tileW, y * tileH)
 
 			end
 		end
 	end
 
-	camera:detach()
+	if drawSign then
 
+		love.graphics.setColor(black)
+		love.graphics.rectangle("fill", player.x - tileW * 2, player.y - tileH * 2, tileW * 15, tileH)
+		love.graphics.setColor(white)
+		love.graphics.setFont(font)
+		love.graphics.print("Hello adventurer", player.x - tileW * 2, player.y - tileH * 2)
+	end
+
+	if drawCantDo then
+		love.graphics.setColor(black)
+		love.graphics.rectangle("fill", player.x - tileW * 2, player.y - tileH * 2, tileW * 15, tileH)
+		love.graphics.setColor(white)
+		love.graphics.setFont(font)
+		love.graphics.print("nothing to use here", player.x - tileW * 2, player.y - tileH * 2) 
+	end
+
+	camera:detach()
+end
 
    
 
-function love.keypressed(key)
-	if key == "up" then
-		if testMapForCollision(0, -1) then 
-		  player.y = player.y - tileH
-		end
-
-	elseif key == "down" then
-		if testMapForCollision(0, 1) then
-			player.y = player.y + tileW
-		end
-
-	elseif key == "left" then
-		if testMapForCollision(-1,0) then
-			player.x = player.x - tileH
-		end
-
-	elseif key == "right" then
-		if testMapForCollision(1,0) then
-			player.x = player.x + tileW
-		end
+function love.keypressed(key)	
 		
-	elseif key == "space" then
-		if testMapForUse(0, -1) then
-			love.graphics.setColor(white)
-			love.graphics.print("Hello adventurer", player.x, player.y - 5) 
+	if key == "a" then
+		if testMapForUse(0, -1) or testMapForUse(0, 1) or testMapForUse(-1, 0) or testMapForUse(1, 0) then
+			drawSign = true
+			
 		else
-			love.graphics.print("nothing to use here", player.x, player.y - 5) 
+			drawCantDo = true
+			
 		end
 
 	end
-end
 
+
+	if key == ("up") then
+		drawSign = false
+		drawCantDo = false
+		if testMapForCollision(0, -1) then 
+		  player.y = player.y - tileW
+		end
+	end
+
+	if key == ("down") then
+		drawSign = false
+		drawCantDo = false
+		if testMapForCollision(0, 1) then
+			player.y = player.y + tileH
+		end
+	end
+
+	if key == ("left") then
+		drawSign = false
+		drawCantDo = false
+		if testMapForCollision(-1,0) then
+			player.x = player.x - tileW
+		end
+	end
+
+	if key == ("right") then
+		drawSign = false
+		drawCantDo = false
+		if testMapForCollision(1,0) then
+			player.x = player.x + tileH
+		end
+	end
 
 
 
